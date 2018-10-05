765bf139b4b55d79b9b346d78331bcf19aa7d8a0
fvictoria <felipevictoria630@yahoo.com>
2018-10-05 11:18:17 -0400

    ...

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student Student1 = new Student(3.5);
+
+    }
+}
---
+++ src/com/company/Student.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by fv799 on 10/5/18.
+ */
+public class Student {
+    public double Student1;
+    public Student (double s1) { Student1 = s1; }
+    public double GPA1 () {
+        double s1 = Student1;
+        return s1;
+    }
+
+}
