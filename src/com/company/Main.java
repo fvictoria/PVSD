be55e38b577cc03deb7f9c4ee51a9df6b6689a99
fvictoria <felipevictoria630@yahoo.com>
2018-10-08 19:45:02 -0400

    I hate Mondays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,50 @@
 package com.company;
-
+import java.sql.SQLOutput;
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Student Student1 = new Student(3.5);
+        Student S1 = new Student("Jae", 7.43);
+        Student S2 = new Student("Thing", 3.65);
+        Student S3 = new Student("Victor", 2.0);
+
+        if (S1.gpa >= S2.gpa && S1.gpa >= S3.gpa && S2.gpa >= S3.gpa) {
+            System.out.println("1:" + S1.name);
+            System.out.println("2:" + S2.name);
+            System.out.println("3:" + S3.name);
+        } else {
+            if (S1.gpa >= S2.gpa && S1.gpa >= S3.gpa && S3.gpa >= S2.gpa) {
+                System.out.println("1:" + S1.name);
+                System.out.println("2:" + S3.name);
+                System.out.println("3:" + S2.name);
+            }
+            else if(S2.gpa >= S1.gpa && S2.gpa >= S3.gpa && S1.gpa >= S3.gpa){
+                System.out.println("1:" + S2.name);
+                System.out.println("2:" + S1.name);
+                System.out.println("3:" + S3.name);
+            }
+            else{
+                if(S2.gpa >= S1.gpa && S2.gpa >= S3.gpa && S3.gpa >= S1.gpa){
+                    System.out.println("1:" + S2.name);
+                    System.out.println("2:" + S3.name);
+                    System.out.println("3:" + S1.name);
+                }
+                else if(S3.gpa >= S2.gpa && S3.gpa >= S1.gpa && S2.gpa >= S1.gpa){
+                    System.out.println("1:" + S3.name);
+                    System.out.println("2:" + S2.name);
+                    System.out.println("3:" + S1.name);
+                }
+                else if(S3.gpa >= S2.gpa && S3.gpa >= S1.gpa && S1.gpa >= S2.gpa){
+                    System.out.println("1:" + S3.name);
+                    System.out.println("2:" + S1.name);
+                    System.out.println("3:" + S2.name);
+                }
+            }
+        }
 
     }
-}
+
+
+
+    }
+
--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -4,11 +4,14 @@ package com.company;
  * Created by fv799 on 10/5/18.
  */
 public class Student {
-    public double Student1;
-    public Student (double s1) { Student1 = s1; }
-    public double GPA1 () {
-        double s1 = Student1;
-        return s1;
+    public String name;
+    public double gpa;
+
+    public Student (String n, double g)
+    {
+        name = n;
+        gpa = g;
     }
 
-}
+    }
+
